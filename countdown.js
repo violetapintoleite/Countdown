cbb3b4b6da94c74eeefd85950369f001f7b39bf0
Violeta Pinto Leite <48143282+violetapintoleite@users.noreply.github.com>
2021-03-11 19:34:44 +0000

    Added more row + changed color and font

--- countdown.js
+++ countdown.js
@@ -1,5 +1,4 @@
-
-  // Set the date we're counting down to
+// Set the first date we're counting down to
   var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
   
   // Update the count down every 1 second
