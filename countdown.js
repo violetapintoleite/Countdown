6928f8400275d79a3d0fdb63181fb4850acb36a6
Violeta Pinto Leite <48143282+violetapintoleite@users.noreply.github.com>
2021-03-15 00:01:00 +0000

    Added countdown to new one

--- countdown.js
+++ countdown.js
@@ -1,5 +1,5 @@
 // Set the FIRST date we're counting down to
-  var countDownDate1 = new Date("Mar 15, 2021 00:00:00").getTime();
+  var countDownDate1 = new Date("Mar 15, 2021 00:01:00").getTime();
   
   // Update the count down every 1 second
   var x = setInterval(function() {
@@ -49,7 +49,7 @@
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
   
     // Display the result in the element with id="demo"
-    document.getElementById("second-demo").innerHTML = days + "dias ";
+    document.getElementById("second-demo").innerHTML = days + "dias " + hours + "h "+ minutes + "m " + seconds + "s ";
   
     // If the count down is finished, write some text
     if (distance < 0) {
